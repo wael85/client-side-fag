c99851d5d42d0a66d3b6000ef325e7bacb6389e5
wael85 <dr.wael85@gmail.com>
2020-03-09 09:11:58 +0100

    new

---
+++ video.js
@@ -0,0 +1,12 @@
+let url = "https://www.youtube.com/embed/zu3wZ-_IKrM";
+
+
+function setSrc(link){
+    link1 = "https://www."+link;
+    url = link1;
+    document.getElementById("video").src =url;
+} 
+
+
+
+
